3d5ce1f179148e375c1a38047c3c37d897bae4fd
Manav Pandey <manavpandey0408@gmail.com>
2021-10-05 13:12:22 +0530

    Anatt ise check karle, ismein abhi bhot changes karne hain, please check

--- script.js
+++
@@ -1,9 +0,0 @@
-$( ".inner-switch" ).on("click", function() {
-    if( $( "body" ).hasClass( "dark" )) {
-      $( "body" ).removeClass( "dark" );
-      $( ".inner-switch" ).text( "OFF" );
-    } else {
-      $( "body" ).addClass( "dark" );
-      $( ".inner-switch" ).text( "ON" );
-    }
-});
